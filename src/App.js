68b7b77cd423cfc16c946d714606e1222725e1e7
Emerson Lopes <lupuselit@outlook.com>
2019-06-30 18:13:25 -0300

    add react examples: functional vs class

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [name, setName] = useState('emerson');
+  function changeName() {
+    setName('Lopes');
+  }
+
+  useEffect(() => {
+    console.log('hello from a functional component');
+  }, []);
+
+  useEffect(() => {
+    console.log('name has changed!');
+  }, [name]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>your name is :{name}</p>
+
+      <button onClick={changeName}>change name!</button>
     </div>
   );
 }
 
-export default App;
+class App2 extends React.Component {
+  state = {
+    name: 'emerson'
+  };
+
+  componentDidMount() {
+    console.log('hello world from a class component');
+  }
+
+  changeName = () => {
+    this.setState({
+      name: 'lopes'
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <p>your name is :{this.state.name}</p>
+
+        <button onClick={this.changeName}>change name!</button>
+      </div>
+    );
+  }
+}
+
+function Main() {
+  return (
+    <>
+      <App />
+      <App2 />
+    </>
+  );
+}
+
+export default Main;
